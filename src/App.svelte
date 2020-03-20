891f618c17970e1d99534e9bb45afe0fc352b3ba
Itai Steinherz <itaisteinherz@gmail.com>
2020-03-20 23:57:29 +0200

    More styling improvements

--- src/App.svelte
+++ src/App.svelte
@@ -34,6 +34,11 @@
 </main>
 
 <style>
+	html, body {
+		height: 100%;
+		overflow-x: hidden;
+	}
+
 	/* We need the grid to position the footer at the bottom of the page */
 	main {
 		height: 100%;
@@ -48,6 +53,21 @@
 		padding-top: 1em;
 		margin: 0 auto;
 	}
+	@media (max-width: 1400px) {
+		main .contents {
+			padding-top: 0.5em;
+		}
+	}
+	@media (max-width: 900px) {
+		main .contents {
+			padding-top: 0.25em;
+		}
+	}
+	@media (max-width: 700px) {
+		main .contents {
+			padding-top: 0;
+		}
+	}
 
 	h1 {
 		color: #ff3e00;
@@ -56,17 +76,11 @@
 		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
-		main .contents {
-			max-width: none;
-		}
-	}
 
 	footer {
 		grid-row-start: 2;
 		grid-row-end: 3;
-		margin-bottom: 10px;
-		padding-bottom: 10px;
+		padding-bottom: 15px;
 		display: flex;
 		flex-direction: row;
 		align-items: center;
